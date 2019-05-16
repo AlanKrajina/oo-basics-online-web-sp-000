6d3a88ae97f1a6691f5406064f7e2d63ce138d7d
Alan Krajina <alankrajina88@gmail.com>
2019-05-16 13:20:59 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -1,8 +1,10 @@
 class Book
-  attr_accessor :title, :author_name, :page_count, :genre
+  attr_accessor :title, :author_name(author), :page_count, :genre
   def initialize (title)
     @title = title
   end
+  
+  
   
 end
 
